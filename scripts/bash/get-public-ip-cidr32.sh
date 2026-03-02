86b3d40989019d4c7e0cf74f90513cd02dd6a550
Shannon Eldridge-Kuehn <shannon.kuehn@gmail.com>
2026-03-01 20:10:02 -0500

    Add author comment to get-public-ip-cidr32.sh
    Added author information to the script.

--- scripts/bash/get-public-ip-cidr32.sh
+++ scripts/bash/get-public-ip-cidr32.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 # Returns your current public IPv4 address as a /32 CIDR.
 # Output: e.g. 203.0.113.7/32
+#
+# Author: Shannon B. Eldridge-Kuehn (2026)
 
 IP="$(curl -fsS https://api.ipify.org)"
 echo "${IP}/32"
